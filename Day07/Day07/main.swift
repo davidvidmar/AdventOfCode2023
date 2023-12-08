ec53d794714bcce0821b5d47bac5bd0800929bd8
David Vidmar <info@vidmar.net>
2023-12-08 17:39:23 +0100

    Day 7 solution

--- Day07/Day07/main.swift
+++ Day07/Day07/main.swift
@@ -3,89 +3,132 @@ import Foundation
 let day = "07"
 print("AoC 2023, Day \(day)\n")
 
-// MARK: load and parse
-print("Parsing: ", terminator: "")
-var startTime = DispatchTime.now()
-
 let path = "/Users/david/Library/CloudStorage/OneDrive-Personal/20-29 Projects/21 Code/21.24 AdventOfCode/AdventOfCode2023-Swift/"
 let content = try String(contentsOfFile: path + "/Day\(day)/Day\(day)/input.txt", encoding: String.Encoding.utf8)
 let lines = content.components(separatedBy: "\n").filter { !$0.isEmpty }
 
-var hands = lines.map { Hand(line: $0) }
-
-print("\(lines.count) lines ", terminator: "")
-print("(\(((DispatchTime.now().uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000)) ms)\n")
-
-// MARK: part 1
-
-print("Part 1: ", terminator: "\n")
-startTime = DispatchTime.now()
+// part 1
 
 var result1 = 0
 
-let sortedHand = hands.sorted(by: { $0.Value < $1.Value })
+var hands = lines.map { Hand(line: $0, jokerRule: false) }
+
+var sortedHand = hands.sorted(by: { $0.Value < $1.Value })
 for (i, hand) in sortedHand.enumerated() {
     //print (hands[0].Cards)
-    print ("\(i+1) * \(hand.Bid) = \((i + 1) * hand.Bid), \(hand.Value)")
+    //print ("\(hand.Name): \(i+1) * \(hand.Bid) = \((i + 1) * hand.Bid), \(hand.Value)")
     result1 += (i+1) * hand.Bid
 }
     
-print("Part 1: \(result1) (\(((DispatchTime.now().uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000)) ms)")
+print("Part 1: \(result1)")
 
-// MARK: part 2
-
-print("Part 2: ", terminator: "")
-startTime = DispatchTime.now()
+// part 2
 
 var result2 = 0
-//...
 
-print("\(result2) (\(((DispatchTime.now().uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000)) ms)\n")
+hands = lines.map { Hand(line: $0, jokerRule: true) }
+
+sortedHand = hands.sorted(by: { $0.Value < $1.Value })
+for (i, hand) in sortedHand.enumerated() {
+//    print ("\(i) - \(hand.Name): \(i+1) * \(hand.Bid) = \((i + 1) * hand.Bid), \(hand.Value)")
+//    print ("\(hand.Name) \(i+1) \(hand.Bid) \(hand.Value)")
+//    print ("\(hand.Name) \(i+1) \(hand.Bid)")
+    result2 += (i+1) * hand.Bid
+}
+
+print("Part 2: \(result2)")
 
 // MARK:
 
 struct Hand {
+    
     var Name: String
+    
     var Bid: Int
     var Cards: [Card]
+    
+    var JokerRule: Bool
     var Value: Int
     
-    init (line: String) {
+    init (line: String, jokerRule: Bool) {
         let str = line.components(separatedBy: " ")
-        self.init(bid: Int(str[1])!, cards: str[0])
+        self.init(bid: Int(str[1])!, cards: str[0], jokerRule: jokerRule)
     }
     
-    init (bid: Int, cards: String, part: Int) {
+    init (bid: Int, cards: String, jokerRule: Bool) {
         
         Bid = bid
-        Cards = cards.enumerated().map { Card(card: $0.element) }
+        Cards = cards.enumerated().map { Card(card: $0.element, jokerRule: jokerRule) }
         Name = cards
-        //print(Name)
+        JokerRule = jokerRule
         
+        let jokers = jokerRule ? Cards.filter { $0.Value == 1 }.count : 0
+        if (jokerRule && jokers > 0)
+        {
+            Cards.removeAll(where: { $0.Value == 1 }) // Joker Out!
+        }
+    
         let counts = Cards.reduce(into: [:]) {
             counts, card in counts[card.Value, default: 0] += 1
         }
-        let maxCount = counts.values.reduce(0, { max($0, $1) })
-        var handValue = maxCount * 100000000000000
-            
-        if maxCount == 2 {
-            // two pairs
-            if (counts.values.filter({ $0 == 2 }).count == 2) { handValue += 50000000000000 }
-        } else if maxCount == 3 {
-            // full house
-            if (counts.values.filter({ $0 == 2 }).count == 1) { handValue += 50000000000000 }
-        } else if maxCount == 4 {
-            
-        } else if maxCount == 5 {
-            
-        } else if maxCount == 1 {
-            
-        } else {
-            //
-            fatalError("tbd")
+        
+        var maxCount = counts.values.max() ?? 0
+        
+        var handValue = 0
+        
+        if (jokerRule) {
+            if (jokers == 5) {
+                maxCount = 5
+            } else if (counts.filter { $0.value == 5 - jokers }.count >= 1) {
+                maxCount = 5
+            } else if (counts.filter { $0.value == 4 - jokers }.count >= 1) {
+                maxCount = 4
+            } else if (counts.filter { $0.value == 2 }.count == 2) {
+                if jokers == 1 {
+                    maxCount = 3
+                    handValue = 50000000000000
+                } // full house
+            } else if (counts.filter { $0.value == 1 }.count == 2) {
+                if jokers == 1 {
+                    maxCount = 3
+                    //handValue = 50000000000000
+                }
+            } else if (counts.filter { $0.value == 3 - jokers }.count >= 1) {
+                maxCount = 3
+            } else {
+                maxCount += jokers == 1 ? 1 : 0
+            }
+        }
+        
+        handValue += maxCount * 100000000000000
+        
+        if true {
+            if maxCount == 5 {
+                
+            } else if maxCount == 4 {
+                
+            } else if maxCount == 3 {
+                if (jokers == 0) {
+                    if (counts.filter { $0.value == 2 }.count == 1) {
+                        handValue += 50000000000000
+                    } // full house
+                }
+            } else if maxCount == 2 {
+                if (jokers == 0) {
+                    if (counts.filter { $0.value == 2 }.count == 2) {
+                        handValue += 50000000000000
+                    } // two pairs
+                }
+            } else if maxCount == 1 {
+                
+            } else if maxCount == 0 {
+                
+            } else {
+                fatalError("tbd")
+            }
         }
 
-        
+        Cards = cards.enumerated().map { Card(card: $0.element, jokerRule: jokerRule) }
         for (i, card) in Cards.enumerated()  {
             handValue += Int(pow(10.0, (Double(10-2*i)))) * card.Value
         }
@@ -97,7 +140,7 @@ struct Hand {
 
 struct Card {
     var Value: Int
-    init (card: Character) {
+    init (card: Character, jokerRule: Bool) {
         switch card {
         case "A":
             Value = 14
@@ -106,8 +149,7 @@ struct Card {
         case "Q":
             Value = 12
         case "J":
-            // Value = 11 - part 1
-            Value = 11
+            Value = jokerRule ? 1 : 11
         case "T":
             Value = 10
         default:
